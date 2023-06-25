7f2bfaf7d70dbe49b8c8bd610e7f8297d1a07884
Gabriel de Morais <morais.g99@gmail.com>
2023-06-25 16:45:57 -0300

    Adicionar arquivos de projeto.

---
+++ ExibirNomesAlunos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ExibirNomesAlunos
+{
+    static void Main()
+    {
+        string[] nomesAlunos = new string[5] { "Gabriel", "Maria", "George", "Breno", "Patricia" };
+
+        Console.WriteLine("Nomes dos alunos:");
+
+        foreach (string nome in nomesAlunos)
+        {
+            Console.WriteLine(nome);
+        }
+    }
+}
